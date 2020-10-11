506319cc2004b5a9190e64d1093af78a79dd0556
SheepTester <ussa.say@gmail.com>
2020-10-11 13:29:58 -0700

    Play around with Mutexes and concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
-use std::env;
+use std::{env, sync::Mutex};
 use dotenv::dotenv;
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
 };
 
-struct Handler;
+struct Handler {
+    count: Mutex<u32>,
+}
 
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+        if msg.content == "ok moofy" {
+            // https://doc.rust-lang.org/book/ch16-03-shared-state.html#sharing-a-mutext-between-multiple-threads
+            let count = {
+                let mut count = self.count.lock().unwrap();
+                *count += 1;
+                count
+            };
+            if let Err(why) = msg.channel_id.say(&ctx.http, count) {
+                println!("Error sending message: {:?}", why);
+            }
+        } else if msg.content == "moofy ponder" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "let me think") {
+                println!("Error sending message: {:?}", why);
+            }
+            std::thread::sleep(std::time::Duration::from_millis(5000));
+            if let Err(why) = msg.channel_id.say(&ctx.http, "done") {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -46,7 +47,9 @@ fn main() {
     // Create a new instance of the Client, logging in as a bot. This will
     // automatically prepend your bot token with "Bot ", which is a requirement
     // by Discord for bot users.
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+    let mut client = Client::new(&token, Handler {
+        count: Mutex::new(0),
+    }).expect("Err creating client");
 
     // Finally, start a single shard, and start listening to events.
     //
